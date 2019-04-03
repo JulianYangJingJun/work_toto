89189f40996a898cae2d01f5b107182a5968baf4
JulianYangJingJun <aqkt_436@163.com>
2019-04-03 11:18:08 +0800

    新增权限文件

---
+++ work_toto/authentication.py
@@ -0,0 +1,11 @@
+
+from django.conf import settings
+from rest_framework.authentication import BaseAuthentication
+
+EXPIRE_MINUTES = getattr(settings, 'REST_FRAMEWORK_TOKEN_EXPIRE_MINUTES', 1)
+
+
+class Authentication(BaseAuthentication):
+
+    def authenticate(self, request):
+        return request.session.get('userid'), None
---
+++ work_toto/permission.py
@@ -0,0 +1,13 @@
+
+from rest_framework.permissions import BasePermission
+
+
+class Permission(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.session.get('userid') is not None:
+            res = True
+        else:
+            res = False
+        return res
+        
