4fc7cd99e117f33edb959bbfdb138d586ba0e5db
JulianYangJingJun <aqkt_436@163.com>
2019-04-03 14:36:01 +0800

    修改代码

--- work_toto/authentication.py
+++ work_toto/authentication.py
@@ -1,6 +1,7 @@
 
 from django.conf import settings
 from rest_framework.authentication import BaseAuthentication
+from django.contrib import auth
 
 EXPIRE_MINUTES = getattr(settings, 'REST_FRAMEWORK_TOKEN_EXPIRE_MINUTES', 1)
 
@@ -8,11 +9,16 @@ EXPIRE_MINUTES = getattr(settings, 'REST_FRAMEWORK_TOKEN_EXPIRE_MINUTES', 1)
 class Authentication(BaseAuthentication):
 
     def authenticate(self, request):
+        return request.session.get('userinfo'), None
+        # user = getattr(request._request, 'userinfo', None)
 
-        user = getattr(request._request, 'userinfo', None)
+        # if not user:
+        #     return None
+        # '''TODO:进一步确认user'''
 
-        if not user:
-            return None
-        '''TODO:进一步确认user'''
+        # return (user, None)
 
-        return (user, None)
+'''
+JulianYangjingjun
+yjj12345
+'''
