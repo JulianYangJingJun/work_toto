f4648442da70874ccb854d862e29ac711b739352
JulianYangJingJun <aqkt_436@163.com>
2019-05-08 16:37:56 +0800

    ip地址验证

--- work_toto/permission.py
+++ work_toto/permission.py
@@ -1,8 +1,13 @@
-
 from rest_framework.permissions import BasePermission
+from config.settings import base as BASE
 
 
 class Permission(BasePermission):
-
     def has_permission(self, request, view):
+        ips = BASE.PERMISSION_HOSTS
+        remote_ip = request.META['REMOTE_ADDR']
+        if remote_ip in ips:
+            return True
+        else:
+            return False
         return bool(request.user)
